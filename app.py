e7a9f3e58df10d2b295557cd9f370013bb95ae0c
Shivam Shrivastava <62115066+Shivam-Shrivastav@users.noreply.github.com>
2023-08-31 12:34:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import requests
 from streamlit_lottie import st_lottie
 
 
-os.environ["OPENAI_API_KEY"] = "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" 
+os.environ["OPENAI_API_KEY"] = "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" 
 EXAMPLE_NO = 1
 
 
@@ -42,7 +42,7 @@ def streamlit_menu(example=1):
 selected = streamlit_menu(example=EXAMPLE_NO)
 
 
-embeddings = OpenAIEmbeddings(openai_api_key='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', model='text-embedding-ada-002')
+embeddings = OpenAIEmbeddings(openai_api_key='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', model='text-embedding-ada-002')
 
 pinecone.init(
     api_key= '4e59065a-e6cd-4b38-a669-89a199057c6f',  # find at app.pinecone.io
